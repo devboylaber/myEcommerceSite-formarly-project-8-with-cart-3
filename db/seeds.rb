bbac7111f03d888d09f1757a706afadb005e4b50
devboylaber <devboy.laber@gmail.com>
2014-12-09 12:33:36 -0500

    c

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@
 end
 1.times do
  Product.create(name: "Tasty Fish", 
-  	             description: "Fried", 
-  	             image: File.new("#{Rails.root}/public/images/atlantic-salmon1.jpg"),
-  	             	price: 3.29,
+                 description: "Fried", 
+                 image: File.new("#{Rails.root}/public/images/atlantic-salmon1.jpg"),
+                  price: 3.29,
                   active: true)
                  # quantity: 1
   # puts "product created"
@@ -48,6 +48,70 @@ end
                   price: 2.99,
                   active: true)
 end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Mmmmm...", 
+                 description: "Supercali", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo1.jpg"),
+                  price: 2.19,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Rainbow trout", 
+                 description: "Eat Me", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo2.jpg"),
+                  price: 7.19,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Guppy", 
+                 description: "Dociousal", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo3.jpg"),
+                  price: 6.29,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "American Flagfish", 
+                 description: "Scrumptious", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo4.jpg"),
+                  price: 8.49,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Black Holly", 
+                 description: "Amazing", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo5.jpg"),
+                  price: 8.29,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Sailfin molly", 
+                 description: "Flayed", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo6.jpg"),
+                  price: 8.39,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Pufferfish", 
+                 description: "Large Peices", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo7.jpg"),
+                  price: 81.49,
+                  active: true)
+end
+                                                      #need to find a pic
+1.times do
+ Product.create(name: "Bumblebee goby", 
+                 description: "Small Peices", 
+                 image: File.new("#{Rails.root}/public/images/stock-photo8.jpg"),
+                  price: 9.49,
+                  active: true)
+end
 
 
 puts "Sample products created"
@@ -60,28 +124,34 @@ Order.create! id: 3
 Order.create! id: 4
 Order.create! id: 5
 Order.create! id: 6
+Order.create! id: 7
+Order.create! id: 8
+Order.create! id: 9
+Order.create! id: 10
+Order.create! id: 11
+Order.create! id: 12
 1.times do |n|
   User.create(email: "user@user.com", 
-  	             password: "useruser",
-  	             password_confirmation: "useruser")
+                 password: "useruser",
+                 password_confirmation: "useruser")
 puts "user created"
 end
 # puts "Sample users created"
 puts "Generating sample users"
 1.times do |n|
   User.create(email: "admin@admin.com", 
-  	             password: "adminadmin",
-  	             password_confirmation: "adminadmin",  
-  	             admin: true)
+                 password: "adminadmin",
+                 password_confirmation: "adminadmin",  
+                 admin: true)
   puts "admin created"
 end
 # puts "Sample admins created"
 puts "Generating sample super admins"
 1.times do |n|
   User.create(email: "super@admin.com", 
-  	             password: "superadmin",
-  	             password_confirmation: "superadmin",  
-  	             super_admin: true)
+                 password: "superadmin",
+                 password_confirmation: "superadmin",  
+                 super_admin: true)
   puts "super admin  created"
 end
 # puts "Sample super admins created"
