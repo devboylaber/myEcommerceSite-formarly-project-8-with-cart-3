72114787a4dd839e73edfcf8144a7ccc3e6a2710
devboylaber <devboy.laber@gmail.com>
2014-12-09 15:59:58 -0500

    added the abilty to destry

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,5 +20,15 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      flash[:notice] = "product deleted!"
+    else
+      flash[:alert] = "There was a problem deleting the product."
+    end
+    redirect_to products_path
+  end
+
 end
 
