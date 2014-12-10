df8c51b505c42cf7b800189255030f69b0b0ca37
devboylaber <devboy.laber@gmail.com>
2014-12-10 12:02:17 -0500

    slight changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,6 @@ class Product < ActiveRecord::Base
   has_attached_file :image, :styles => { :huge => "500x500>", :large => "400x400>", :medium => "300x300>", :small => "200x200>", :tiny => "100x100>" }, :defualt_url => "/images/:style/cool.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/  
 
-  has_attached_file :slideimage, :styles => { :huge => "500x500>", :large => "400x400>", :medium => "300x300>", :small => "200x200>", :tiny => "100x100>" }, :defualt_url => "/slideimages/:style/cool.jpg"
-  validates_attachment_content_type :slideimage, :content_type => /\Aslideimage\/.*\Z/  
 
   default_scope { where(active: true) }
 end
